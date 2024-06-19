1db41dbda88099cc62b7ec140f5ff8b7bfed841f
Ahnjaesung <voyagerahn@gmail.com>
2024-06-19 21:15:04 +0900

    Change password

--- app/src/main/java/io/falab/samsungbarunhospital/MainActivity.java
+++ app/src/main/java/io/falab/samsungbarunhospital/MainActivity.java
@@ -114,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
                     builder.setPositiveButton("확인",
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
-                                    if ("1872".equals(input.getText().toString())) {
+                                    if ("0819".equals(input.getText().toString())) {
                                         Toast.makeText(getApplicationContext(), "설정 진입", Toast.LENGTH_SHORT).show();
                                         Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -163,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
                     builder.setPositiveButton("확인",
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
-                                    if ("1872".equals(input.getText().toString())) {
+                                    if ("0819".equals(input.getText().toString())) {
                                         startNewActivity(MainActivity.this, "com.android.launcher3");
                                     } else {
                                         Toast.makeText(getApplicationContext(), "비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
